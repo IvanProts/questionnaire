afedb834b018e5a7772c6ceb78bf8c66bd25f56c
ivan.prots <ivan.prots@oril.co>
2025-03-17 21:24:06 +0200

    QUE-3/added prettier and setuped eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,26 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:import/recommended",
+    "plugin:prettier/recommended"
+  ),
+  {
+    rules: {
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'bem': ['warn', { ignoreClassNames: ['^bg-\\[.*\\]$'] }],
+      'import/order': [
+        'warn',
+        {
+          groups: [['builtin', 'external'], ['internal', 'parent', 'sibling', 'index']],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
